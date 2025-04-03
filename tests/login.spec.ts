646aa85bd3458928a9bdcd899600130acc042738
gitaashishk <162989959+gitaashishk@users.noreply.github.com>
2025-04-03 18:15:04 +0530

    Update login.spec.ts

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -7,8 +7,8 @@ import { CartPage } from '../pages/CartPage';
 import { CheckoutPage } from '../pages/CheckoutPage';
 
 test.describe('Login and Checkout Tests', () => {
-  loginData.forEach((data,i) => {
-    test(`Test ${i + 1} for user : ${data.username}`, async ({ page }) => {
+  loginData.forEach((data, i) => {
+    test(`Test ${i + 1} for user: ${data.username}`, async ({ page }) => {
       const loginPage = new LoginPage(page);
       await loginPage.navigate();
       await loginPage.login(data.username, data.password);
@@ -46,6 +46,54 @@ test.describe('Login and Checkout Tests', () => {
     });
   });
 });
+
+// Test for blank delivery details
+test.describe('Blank Delivery Details', () => {
+  checkoutdata.forEach((data, i) => {
+    test(`Test ${i + 1} Blank Delivery details for user: ${data.username}`, async ({ page }) => {
+      const loginPage = new LoginPage(page);
+      await loginPage.navigate();
+      await loginPage.login(data.username, data.password);
+
+      if (data.shouldLogin) {
+        const success = await loginPage.isLoginSuccessful();
+        expect(success).toBe(true);  // Explicitly fail if false
+
+        if (success && data.checkoutItems && data.checkoutInfo && data.TestCase === 'emptydeliverydetails') {
+          const inventoryPage = new InventoryPage(page);
+          for (const item of data.checkoutItems) {
+            await inventoryPage.addItemToCart(item);
+          }
+          await inventoryPage.navigateToCart();
+
+          const cartPage = new CartPage(page);
+          await cartPage.checkout();
+
+          const checkoutPage = new CheckoutPage(page);
+          const info = data.checkoutInfo;
+          await checkoutPage.fillCheckoutInfo(info.firstName, info.lastName, info.zipCode);
+
+          if (page.url() === 'https://www.saucedemo.com/checkout-step-two.html') {
+            await checkoutPage.completeCheckout();
+            const orderSuccess = await checkoutPage.isOrderSuccessful();
+            expect(orderSuccess).toBe(true);
+          } else {
+            console.log('Cannot place order due to invalid details');
+            const checkouterrorText = await checkoutPage.invalidcheckoutdetails();
+            expect(checkouterrorText).toContain('First Name is required');
+          }
+        }
+
+      } else {
+        const errorDisplayed = await loginPage.isLoginError();
+        expect(errorDisplayed).toBe(true);  // Explicitly fail if error not displayed
+
+}
+
+	}
+  }
+});
+
 
 // Test for blank delivery details
 test.describe('Blank Delivery Details', () => {
